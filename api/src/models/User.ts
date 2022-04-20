1fd4287eafdf176dc3363855ef4fab7e9488e68e
farid555 <farid555a@gmail.com>
2022-04-21 00:06:38 +0300

    created backend api

---
+++ api/src/models/User.ts
@@ -0,0 +1,50 @@
+import mongoose, { Date, Document } from 'mongoose'
+
+export type UserDocument = Document & {
+  firstName: string
+  lastName: string
+  email: string
+  phone: string
+  address: string
+  borrowBook: {
+    bookId: string
+    getBorrow: Date
+    returnBook: Date
+  }[]
+}
+
+const bookSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  borrowBook: [
+    {
+      bookId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Book',
+      },
+      getBorrow: Date,
+      returnBook: Date,
+    },
+  ],
+})
+
+export default mongoose.model<UserDocument>('User', bookSchema)
